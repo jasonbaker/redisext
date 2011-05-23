b427a6ce8c0886e383c715d105c44c72890ac3fa
Jason Baker <jason@apture.com>
2011-05-23 14:48:43 -0700

    Adding sharded connection.

---
+++ redisext/shard.py
@@ -0,0 +1,33 @@
+from __future__ import absolute_import
+import hashlib
+import sys
+
+class ShardedRedis(object):
+    """
+    A wrapper around a Redis object that supports sharding.
+    """
+    def __init__(self, redis_mapping):
+        self.redis_mapping = redis_mapping
+
+    def _getconnection(self, name):
+        h = hashlib.md5().digest()[0]
+        h = ord(h)
+        index = h % len(self.redis_mapping)
+        keys = self.redis_mapping.keys()
+        keys.sort()
+        key = keys[index]
+        return self.redis_mapping[key]
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        for r in self.redis_mapping.values():
+            r.connection.disconnect()
+
+    def __getattr__(self, name):
+        def _wrapper(key, *args, **kwargs):
+            connection = self._getconnection(key)
+            method = getattr(connection, name)
+            return method(key, *args, **kwargs)
+        return _wrapper
---
+++ tests/test_shard.py
@@ -0,0 +1,18 @@
+from redisext.shard import ShardedRedis
+
+import mock
+
+connections = {'a' : mock.Mock(),
+               'b' : object(),
+               'c' : object(),
+               'd' : object()}
+
+def test_connection():
+    r = ShardedRedis(connections)
+    actual_connection = r._getconnection("foo")
+    assert actual_connection == connections['a']
+
+def test_getattr():
+    r = ShardedRedis(connections)
+    r.get('foo')
+    assert connections['a'].get.called
